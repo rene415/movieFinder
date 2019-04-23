e89f072b0ae71b3c214a7928392b253e270944f6
rene415 <ramirezrene96@gmail.com>
2019-04-22 23:27:27 -0700

    added name of movie in english and spanish

--- scrape.js
+++ scrape.js
@@ -3,7 +3,7 @@ const
     cheerio = require('cheerio'),
     fs = require('fs');
 
-fs.appendFileSync('results.csv',`________\n`);
+fs.appendFileSync('horror.csv',`________\n`);
 
 function checkNum(title){
     var number = parseInt(title);
@@ -17,13 +17,25 @@ function ft_grabMovie(link, title){
     if(link != undefined && title != "→" && title != "←" && checkNum(title) != 'number'){
         request(link, (error, response, html) =>{
             if (!error && response.statusCode == 200){
-                const
-                    $ = cheerio.load(html);
+                var
+                    $ = cheerio.load(html),
+                    englishTitle = "",
+                    yearOfMovie = "";
+
+                $('.extras').each(function(i, el){
+                    yearOfMovie = $(el).text().replace(/\s\s+/g, '');
+                    yearOfMovie = yearOfMovie.substring(yearOfMovie.length-4)
+                    yearOfMovie = "("+yearOfMovie+")";
+                });
+                $('.ori_title').each(function(i, el){
+                    englishTitle = $(el).text();
+                });
+                
                 $('.green').each(function(i, el){
                     const
                         torrentLink = $(el).attr('href');
                 if(link != "http://playtorrent.org/estrenos" && link != "http://playtorrent.org/mejor-valoradas" && link != undefined && title != /\d/g){
-                    fs.appendFileSync('results.csv',`${title},${torrentLink}\n`);
+                    fs.appendFileSync('horror.csv',`${englishTitle +" "+yearOfMovie},${title},${torrentLink},${englishTitle}\n`);
                 }
                 });  
             }
@@ -31,14 +43,15 @@ function ft_grabMovie(link, title){
     };
 }
 
-request('http://playtorrent.org/page/10/', (error, response, html) =>{
+request('https://playtorrent.org/generos/terror/page/20/', (error, response, html) =>{
     if (!error && response.statusCode == 200){
         const
             $ = cheerio.load(html);
         $('.containerxbusca a').each(function(i, el){
             const
                 title = $(el).text().replace(/\s\s+/g, ''),
-                link = $(el).attr('href');      
+                link = $(el).attr('href');   
+                
             ft_grabMovie(link, title);
         });
         console.log("\nScrape is done\n");
